f50e3082ec6843755ec777c576e5facc3c12a693
valkappach <valantinkit@gmail.com>
2021-04-23 11:36:41 +0200

    first commit

---
+++ src/main/java/com/kcbooking/kchomebooking/KcHomeBookingApplication.java
@@ -0,0 +1,13 @@
+package com.kcbooking.kchomebooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcHomeBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KcHomeBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kcbooking/kchomebooking/KcHomeBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kcbooking.kchomebooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcHomeBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
